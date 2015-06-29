f2ccefad25731d16524b4c6ba9849307b3f367fa
akashbudhia <akashbudhia22@gmail.com>
2015-06-29 14:04:52 +0530

    Payoof data in participant schema

--- otree/views/admin.py
+++ otree/views/admin.py
@@ -38,7 +38,67 @@ from otree.models.session import Session, Participant
 
 from django.core import serializers 
 import json
+from django.core.serializers.base import Serializer as BaseSerializer
+from django.core.serializers.python import Serializer as PythonSerializer
+from django.core.serializers.json import Serializer as JsonSerializer
+from django.utils import six
 
+class ExtBaseSerializer(BaseSerializer):
+    """ Abstract serializer class; everything is the same as Django's base except from the marked lines """
+    def serialize(self, queryset, **options):
+        self.options = options
+
+        self.stream = options.pop('stream', six.StringIO())
+        self.selected_fields = options.pop('fields', None)
+        self.selected_props = options.pop('props', None)  # added this
+        self.use_natural_keys = options.pop('use_natural_keys', False)
+        self.use_natural_foreign_keys = options.pop('use_natural_foreign_keys', False)
+        self.use_natural_primary_keys = options.pop('use_natural_primary_keys', False)
+
+        self.start_serialization()
+        self.first = True
+        for obj in queryset:
+            self.start_object(obj)
+            concrete_model = obj._meta.concrete_model
+            for field in concrete_model._meta.local_fields:
+                if field.serialize:
+                    if field.rel is None:
+                        if self.selected_fields is None or field.attname in self.selected_fields:
+                            self.handle_field(obj, field)
+                    else:
+                        if self.selected_fields is None or field.attname[:-3] in self.selected_fields:
+                            self.handle_fk_field(obj, field)
+            for field in concrete_model._meta.many_to_many:
+                if field.serialize:
+                    if self.selected_fields is None or field.attname in self.selected_fields:
+                        self.handle_m2m_field(obj, field)
+            # added this loop
+            for field in self.selected_props:
+                self.handle_prop(obj, field)
+            self.end_object(obj)
+            if self.first:
+                self.first = False
+        self.end_serialization()
+        return self.getvalue()
+
+    # added this function
+    def handle_prop(self, obj, field):
+        self._current[field] = getattr(obj, field)()
+
+
+class ExtPythonSerializer(ExtBaseSerializer, PythonSerializer):
+    pass
+
+
+class ExtJsonSerializer(ExtPythonSerializer, JsonSerializer):
+    pass
+
+
+
+def getSerializableObjectWithAttributes(obj, isList = False, props = None):
+    data = ExtJsonSerializer().serialize((obj if isList else [obj,]), props = props)
+    struct = json.loads(data)
+    return struct if isList else struct[0]
 
 def getSerializableObject(obj, isList = False):
     data = serializers.serialize('json', (obj if isList else [obj,]))
@@ -555,7 +615,7 @@ class SessionPayments(AdminSessionPageMixin, vanilla.TemplateView):
         # dictionary for json response
         # will be used only if json request  is done
         self.context_json = {
-            'participants': getSerializableObject(participants, True),
+            'participants': getSerializableObjectWithAttributes(participants, True, props=['payoff_in_real_world_currency','money_to_pay','money_to_pay_display','payoff_in_real_world_currency']),
             'total_payments': total_payments,
             'mean_payment': mean_payment,
             'participation_fee': session.participation_fee,
